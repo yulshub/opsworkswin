94eb84aa047650416009c87cd494cf74fda01bcf
Yuls White <Yuls@iMac-Yuls.local>
2016-07-24 13:12:18 +0200

    testing

--- deployer/recipes/deployapp.rb
+++ deployer/recipes/deployapp.rb
@@ -20,6 +20,30 @@ apps.each do |app|
 
   app_source = app["app_source"]
   app_checkout = ::File.join(Chef::Config["file_cache_path"], app["shortname"])
+  opsworks_scm_checkout app["shortname"] do
+    destination      app_checkout
+    repository       app_source["url"]
+    revision         app_source["revision"]
+    user             app_source["username"]
+    password         app_source["password"]
+    ssh_key          app_source["ssh_key"]
+    type             app_source["type"]
+  end
+
+  app_deploy = ::File.join(node["deployer"]["deploydir"], app["shortname"])
+
+  directory app_deploy do
+    action :create
+    recursive true
+    rights :full_control, "IIS_IUSRS", :applies_to_children => true, :applies_to_self => true if platform?("windows")
+  end
+
+# Copy app to deployment directory
+  batch "copy #{app["shortname"]}" do
+    code "Robocopy.exe #{app_checkout} #{app_deploy} /MIR /XF .gitignore /XF web.config.erb /XD .git"
+    returns (0..7).to_a
+  end
+
 
 end
 
