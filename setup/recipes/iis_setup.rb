f39ee234a1882892906546de0d038717c15ec20f
Yuls White <Yuls@iMac-Yuls.local>
2016-07-24 09:44:45 +0200

    iis test 1

---
+++ setup/recipes/iis_setup.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: setup
+# Recipe:: iis_setup
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# Enable the IIS role.
+dsc_script 'Web-Server' do
+  code <<-EOH
+  WindowsFeature InstallWebServer
+  {
+    Name = "Web-Server"
+    Ensure = "Present"
+  }
+  EOH
+end
+
+# Install ASP.NET 4.5.
+dsc_script 'Web-Asp-Net45' do
+  code <<-EOH
+  WindowsFeature InstallDotNet45
+  {
+    Name = "Web-Asp-Net45"
+    Ensure = "Present"
+  }
+  EOH
+end
+
+
+# Install the IIS Management Console.
+dsc_script 'Web-Mgmt-Console' do
+  code <<-EOH
+  WindowsFeature InstallIISConsole
+  {
+    Name = "Web-Mgmt-Console"
+    Ensure = "Present"
+  }
+  EOH
+end
+
+
+
+
