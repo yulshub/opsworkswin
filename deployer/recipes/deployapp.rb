870343ce4ab405fad438c76a1cb12462e542bfd3
Yuls White <Yuls@iMac-Yuls.local>
2016-07-24 13:15:24 +0200

    testing

--- deployer/recipes/deployapp.rb
+++ deployer/recipes/deployapp.rb
@@ -9,17 +9,17 @@ log 'message' do
   level :info
 end
 
-Chef::Log.info "About to search apps"
+Chef::Log.info "Buscando aplicaciones"
 
-# Search apps to be deployed. Without deploy:true filter all apps would be returned.
 apps = search(:aws_opsworks_app, "deploy:true") rescue []
-Chef::Log.info "Found #{apps.size} apps to deploy on the stack. Assuming they are all Node.JS apps."
+Chef::Log.info "Encontradas #{apps.size} apps para deploy en el stack."
 
 apps.each do |app|
-  Chef::Log.info "Deploying #{app["shortname"]}."
+  Chef::Log.info "Desplegando #{app["shortname"]}."
 
   app_source = app["app_source"]
   app_checkout = ::File.join(Chef::Config["file_cache_path"], app["shortname"])
+  
   opsworks_scm_checkout app["shortname"] do
     destination      app_checkout
     repository       app_source["url"]
@@ -38,23 +38,10 @@ apps.each do |app|
     rights :full_control, "IIS_IUSRS", :applies_to_children => true, :applies_to_self => true if platform?("windows")
   end
 
-# Copy app to deployment directory
+# Copy app
   batch "copy #{app["shortname"]}" do
-    code "Robocopy.exe #{app_checkout} #{app_deploy} /MIR /XF .gitignore /XF web.config.erb /XD .git"
+    code "Robocopy.exe #{app_checkout} #{app_deploy} /MIR"
     returns (0..7).to_a
   end
 
-
 end
-
-
-
-
-
-#node[:deploy].each do |app_name, deploy|
-#   file "c:\\inetput\\wwwroot\\index.html" do
-#      content IO.read("#{deploy[:deploy_to]}\\current\\index.html")
-#      action :create
-#   end
-#end
-
