ef52f8c500fe6272d7824c983c454357217cd814
Yuls White <Yuls@iMac-Yuls.local>
2016-07-24 11:47:55 +0200

    Add deploy, sin mucha fe

---
+++ deploy/metadata.rb
@@ -0,0 +1,7 @@
+name 'deploy'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures deploy'
+long_description 'Installs/Configures deploy'
+version '0.1.0'
---
+++ deploy/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: deploy
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
